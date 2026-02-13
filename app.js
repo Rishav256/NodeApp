a4e3b55dd8c8fcaefc4a0d725c589a6d3bd51d30
Rishav256 <naiya.rishav.18@gmail.com>
2026-02-13 11:19:56 +0530

    Refactor project structure to follow MVC architecture

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin.js');
+const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
+
+const errorControl = require('./controllers/errorController.js');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page not found', path: '' });
-});
+app.use(errorControl.get404Page);
 
 app.listen(3000);
