37eb32245cb8a8c378cade465910db3dac6d911d
Rishav256 <naiya.rishav.18@gmail.com>
2026-05-05 13:54:30 +0530

    Dynamic routes added

---
+++ models/cartModel.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(path.dirname(require.main.filename), 'data', 'cart.json');
+
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    //fetch previous cart
+    fs.readFile(p, (err, fileContent) => {
+      let cart = {products: [], totalPrice = 0};
+      if(err) {
+        cart = JSON.parse(fileContent);
+      }
+    });
+
+    //analyze -> find existing product
+    const existingProduct = cart.products.find(prod=>prod.id === id);
+    let updatedProduct;
+    if(existingProduct) {
+      updatedProduct = {...existingProduct};
+      updatedProduct.qty = updatedProduct.qty+1;
+    } else {
+      updatedProduct = {id = id, qty:1};
+    }
+    //add new product/increase quantity
+
+    cart.totalPrice = cart.totalPrice + productPrice;
+  }
+};
