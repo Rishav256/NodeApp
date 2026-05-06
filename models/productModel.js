8fdd535aaa5de3105e6e80d32d2af1da47af105f
Rishav256 <naiya.rishav.18@gmail.com>
2026-05-06 15:08:59 +0530

    Added editing functionality

--- models/productModel.js
+++ models/productModel.js
@@ -18,19 +18,31 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title, imgUrl, description, price) {
+  constructor(id, title, imgUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imgUrl = imgUrl;
     this.description = description;
     this.price = price;
   }
   save() {
-    this.id = Math.random();
     getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          (prod) => prod.id === this.id,
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
     });
   }
 
